78e10edd64f28aa97c4145693e03cfadc92ee7dc
Nikita12112002 <guptaniki488@gmail.com>
2021-06-18 22:39:44 +0530

    eight commit

---
+++ Day-8/100days.js
@@ -0,0 +1,63 @@
+ 
+document.write("<table>");
+for(i=0 ;i<17 ;i++)
+{
+    fun();
+    
+}
+
+ function fun()
+ {
+
+ var row ="<tr>";
+ var col = "<td>";
+ 
+ 
+for(i=0 ;i<17 ;i++)
+{
+   document.write(row);
+   console.log(row);
+    
+    for(j=0 ;j<61;j++)
+    {
+       document.write(col);
+       console.log(col);
+    }
+
+}
+ }
+
+ document.write("</table>");
+
+ //................................................................
+
+
+ var s = document.querySelectorAll('td');
+
+  s.forEach(item=>
+   {
+      item.addEventListener("click",function()
+      {
+         item.classList.toggle("white");
+      })
+   }
+   )
+
+ /*var col="<td id='";
+
+ for(i=0 ;i<17 ; i++)
+ {
+   
+    for(j=0 ;j<61;j++)
+     {
+        var p = table[i][j];
+        console.log(p);
+
+ function color()
+ {
+    p.classList.toggle("white");
+    console.log("clicked");
+
+ }
+}
+}*/
